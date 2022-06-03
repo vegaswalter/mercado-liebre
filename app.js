61f1faa407b0204d28bd2ec5177e2d4293d1682d
vegaswalter <damianvegasrodriguez@gmail.com>
2022-06-02 23:47:56 -0300

    arreglo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Servidor
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
